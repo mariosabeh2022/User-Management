18ea87e4e1bab9077d9e5a996ffc2091747ceb51
mariosabeh2022 <mariosabeh2022@gmail.com>
2025-04-03 19:20:59 +0300

    Added margin to Search component for better spacing

--- user-management-interface/src/Components/Atoms/Search.tsx
+++ user-management-interface/src/Components/Atoms/Search.tsx
@@ -2,7 +2,7 @@
 const Search=()=>{
     return (
         <input type="text"
-               className="m-8 text-black px-2 py-2 border border-gray-300 rounded-sm"
+               className="mt-8 ml-8  text-black px-2 py-2 border border-gray-300 rounded-sm"
                placeholder="Search users..."/>
     )
 }
