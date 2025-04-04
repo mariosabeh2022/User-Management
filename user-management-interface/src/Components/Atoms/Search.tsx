dcd681838cad676aa9095e6f3847e5aa1941602b
mariosabeh2022 <mariosabeh2022@gmail.com>
2025-04-05 02:11:10 +0300

    Adjusted the search component size

--- user-management-interface/src/Components/Atoms/Search.tsx
+++ user-management-interface/src/Components/Atoms/Search.tsx
@@ -1,7 +1,7 @@
 const Search=()=>{
     return (
         <input type="text"
-               className="mt-8 ml-8  text-black px-2 py-2 border border-gray-300 rounded-sm"
+               className="mt-8 ml-8 text-black px-4 py-2 border border-gray-300 rounded-sm"
                placeholder="Search users..."
         />
     )
