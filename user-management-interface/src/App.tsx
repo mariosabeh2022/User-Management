234f512281e3f9e2a3ffaab7354e64d21b4f54f2
mariosabeh2022 <mariosabeh2022@gmail.com>
2025-04-10 16:43:43 +0300

    Added loading screen with a timeout of 1 second

--- user-management-interface/src/App.tsx
+++ user-management-interface/src/App.tsx
@@ -1,7 +1,17 @@
 import './App.css'
 import HomePage from './Components/Pages/HomePage'
+import LoadingPage from './Components/Pages/LoadingPage'
+import {useEffect,useState} from 'react'
+
 function App() {
-  return <HomePage/>
+  const [isLoading, setIsloading]=useState(true);
+  useEffect(()=>{
+    setTimeout(()=>{
+      setIsloading(false)
+    }, 1000)
+  },[])
+  if(isLoading) return <LoadingPage/>
+  else return <HomePage/>
 }
 
 export default App
